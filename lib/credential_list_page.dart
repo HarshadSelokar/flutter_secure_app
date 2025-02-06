0ca8c83a7f47fcb13562d0a8abacc1838ca47b00
Harshad Selokar <anujselokar7@gmail.com>
2025-02-06 12:49:06 +0530

    project commited

--- lib/credential_list_page.dart
+++ lib/credential_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'credential_detail_page.dart';
 
 class CredentialListPage extends StatefulWidget {
@@ -9,40 +10,47 @@ class CredentialListPage extends StatefulWidget {
 }
 
 class _CredentialListPageState extends State<CredentialListPage> {
-  List<Map<String, String>> credentials = [
-    {'service': 'Gmail', 'username': 'user@gmail.com', 'password': '********'},
-    {'service': 'Spotify', 'username': 'spotify_user', 'password': '********'},
-  ];
+  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+  List<Map<String, String>> credentials = [];
 
-  void handleEdit(int index, dynamic result) {
-    if (result is Map<String, String>) {
-      setState(() {
-        credentials[index] = {
-          'service': credentials[index]['service']!,
-          'username': result['username']!,
-          'password': result['password']!,
-        };
+  @override
+  void initState() {
+    super.initState();
+    _loadCredentials();
+  }
+
+  Future<void> _loadCredentials() async {
+    final allKeys = await secureStorage.readAll();
+    List<Map<String, String>> loadedCredentials = [];
+
+    allKeys.forEach((key, value) {
+      loadedCredentials.add({
+        'service': key,
+        'username': value.split('|')[0],
+        'password': value.split('|')[1],
       });
-    } else if (result == 'deleted') {
-      setState(() {
-        credentials.removeAt(index);
-      });
-    }
+    });
+
+    setState(() {
+      credentials = loadedCredentials;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Your Credentials")),
-      body: ListView.builder(
+      body: credentials.isEmpty
+          ? const Center(child: Text("No credentials saved yet."))
+          : ListView.builder(
         itemCount: credentials.length,
         itemBuilder: (context, index) {
           final credential = credentials[index];
           return ListTile(
             title: Text(credential['service']!),
             subtitle: Text(credential['username']!),
-            onTap: () async {
-              final result = await Navigator.push(
+            onTap: () {
+              Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => CredentialDetailPage(
@@ -52,7 +60,6 @@ class _CredentialListPageState extends State<CredentialListPage> {
                   ),
                 ),
               );
-              handleEdit(index, result);
             },
           );
         },
